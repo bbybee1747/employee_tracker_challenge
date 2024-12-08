ac3a75d3d5763a796401704142b4b9512d001e64
Brandon Bybee <bybee.brandon1@gmail.com>
2024-12-08 08:38:32 -0600

    fixing some issues with file locations

--- src/db.ts
+++ src/db.ts
@@ -4,9 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db = new Client({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME,
     port: parseInt(process.env.DB_PORT || '5432', 10),
 });
